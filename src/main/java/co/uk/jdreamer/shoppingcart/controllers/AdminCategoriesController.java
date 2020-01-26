661710047d9265536efaa1ce541e8457bbd2b8ab
unknown <dhramijo@github.com>
2020-01-26 21:42:35 +0000

    Added Category reording

--- src/main/java/co/uk/jdreamer/shoppingcart/controllers/AdminCategoriesController.java
+++ src/main/java/co/uk/jdreamer/shoppingcart/controllers/AdminCategoriesController.java
@@ -3,15 +3,11 @@ package co.uk.jdreamer.shoppingcart.controllers;
 import co.uk.jdreamer.shoppingcart.models.Category;
 import co.uk.jdreamer.shoppingcart.models.Page;
 import co.uk.jdreamer.shoppingcart.repositories.CategoryRepository;
-import co.uk.jdreamer.shoppingcart.repositories.PageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
@@ -27,7 +23,7 @@ public class AdminCategoriesController {
     @GetMapping
     public String index(Model model) {
 
-        List<Category> categories = categoryRepository.findAll();
+        List<Category> categories = categoryRepository.findAllByOrderBySortingAsc();
         // Pass the list of pages to index view
         model.addAttribute("categories", categories);
 
@@ -64,5 +60,73 @@ public class AdminCategoriesController {
         }
 
         return "redirect:/admin/categories/add";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable int id, Model model) {
+
+        Category category = categoryRepository.getOne(id);
+
+        model.addAttribute("category", category);
+
+        return "admin/categories/edit";
+
+    }
+
+    @PostMapping("/edit")
+    public String edit(@Valid Category category, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        Category currentCategory = categoryRepository.getOne(category.getId());
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("categoryName", currentCategory.getName());
+            return "admin/categories/edit";
+        }
+
+        redirectAttributes.addFlashAttribute("message", "Category edited");
+        redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+
+        String slug = category.getName().toLowerCase().replace(" ", "-");
+        Category categoryExists = categoryRepository.findByName(category.getName());
+
+        if (categoryExists != null) {
+            redirectAttributes.addFlashAttribute("message", "Category exists, choose another");
+            redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+        } else {
+            category.setSlug(slug);
+            categoryRepository.save(category);
+        }
+
+        return "redirect:/admin/categories/edit/" + category.getId();
+
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable int id, RedirectAttributes redirectAttributes) {
+
+        categoryRepository.deleteById(id);
+
+        redirectAttributes.addFlashAttribute("message", "Category deleted");
+        redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+
+        return "redirect:/admin/categories";
+
+    }
+
+    @PostMapping("/reorder")
+    public @ResponseBody
+    String reorder(@RequestParam("id[]") int[] id) {
+
+        int count = 1;
+        Category category;
+
+        for (int categoryId : id) {
+            category = categoryRepository.getOne(categoryId);
+            category.setSorting(count);
+            categoryRepository.save(category);
+            count++;
+        }
+
+        return "ok";
     }
 }
--- src/main/java/co/uk/jdreamer/shoppingcart/repositories/CategoryRepository.java
+++ src/main/java/co/uk/jdreamer/shoppingcart/repositories/CategoryRepository.java
@@ -3,7 +3,11 @@ package co.uk.jdreamer.shoppingcart.repositories;
 import co.uk.jdreamer.shoppingcart.models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+    List<Category> findAllByOrderBySortingAsc();
 
     Category findByName(String name);
 
