2214afc778d9f613e419203838446331fb586608
dhramijo <dhrami86@yahoo.it>
2020-03-29 18:49:01 +0100

    Added Users, Admin tables and Entities

---
+++ src/main/java/co/uk/jdreamer/shoppingcart/models/Admin.java
@@ -0,0 +1,51 @@
+package co.uk.jdreamer.shoppingcart.models;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Arrays;
+import java.util.Collection;
+
+@Entity
+@Table(name = "admin")
+@Data
+public class Admin implements UserDetails {
+
+    private static final long serialVersionUID = 2l;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String username;
+
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
